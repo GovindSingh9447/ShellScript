7d6c6733af79e40f3180c39fc97d9396ed714bc5
GovindSingh <govindkrsingh0605@gmail.com>
2022-12-12 21:27:48 +0530

    variable

---
+++ Define_Variable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#user Define Variables.
+name="Saam"
+age="30"
+
+#system define variables.
+echo $(name)
+echo "My name is ${name} and my age is ${age}"
+work="program"
+var="ing"
+echo "I am ${work}${var}"
+
+
+
+
+
+
