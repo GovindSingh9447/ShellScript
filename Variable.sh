f54c2a20155efbd5f1aaba0163a04b4130b58c33
GovindSingh <govindkrsingh0605@gmail.com>
2022-12-12 21:08:53 +0530

    first commit

---
+++ Variable.sh
@@ -0,0 +1,15 @@
+
+
+_variableNAME="First Name"
+
+variable3Name="Second Name"
+
+name="govind"
+Name="Singh"
+aAME="amt"
+
+
+echo "${name} ${Name} ${aAME}"
+echo "${_variableNAME}"
+
+echo "${variable3Name}"
